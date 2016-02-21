bcf73721b88c4721bcb0c96c9f17d029b31689bc
mmmeyers <marsi286@gmail.com>
2016-02-21 15:59:03 -0500

    Done.

---
+++ app/controllers/settings_controller.rb
@@ -0,0 +1,7 @@
+class SettingsController < ApplicationController
+  def index
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
 
+  get 'access/index'
+
+  get 'access/update'
+
+  get 'index/update'
+
+  get 'settings/index'
+
+  get 'settings/update'
+
+  get 'settings_controller/index'
+
+  get 'settings_controller/update'
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+
   resources :songs
+
+  namespace :admin do
+    resources :settings, only: [:index, :update]
+    resources :access, only: [:index, :update]
+  end
 
 end
